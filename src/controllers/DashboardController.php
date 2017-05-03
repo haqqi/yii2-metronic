c4dcbad9c4e719c4b32c1ba58accfc2a749c8967
Haqqi <me@haqqi.net>
2017-05-03 18:50:00 +0700

    [add] Dashboard controller for demo

---
+++ src/controllers/DashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace haqqi\metronic\controllers;
+
+use yii\web\Controller;
+
+class DashboardController extends Controller
+{
+    public function actionIndex() {
+        echo 'dashboard';
+    }
+}
