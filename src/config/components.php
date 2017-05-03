818138146208cd0f565b28a10a4f5e82b53efb9b
Haqqi <me@haqqi.net>
2017-05-03 18:49:46 +0700

    [add] Bootstrap mechanism

---
+++ src/config/components.php
@@ -0,0 +1,11 @@
+<?php
+
+use yii\web\View;
+
+return [
+    'yii\web\JqueryAsset' => [
+        'sourcePath' => null, // do not publish the file
+//        'js'         => ['jquery.min.js'],
+//        'jsOptions'  => ['position' => View::POS_HEAD],
+    ],
+];
