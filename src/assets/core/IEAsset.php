de209dd23bb89649bfb2942f92fea368d496edf4
Haqqi <me@haqqi.net>
2017-05-07 14:25:30 +0700

    [add] IEAsset

---
+++ src/assets/core/IEAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace haqqi\metronic\assets\core;
+
+use haqqi\metronic\base\assets\GlobalPluginAssetBundle;
+use yii\web\View;
+
+class IEAsset extends GlobalPluginAssetBundle
+{
+    public $js = [
+        'respond.min.js',
+        'excanvas.min.js',
+        'ie8.fix.min.js'
+    ];
+
+    public $jsOptions = [
+        'position' => View::POS_HEAD,
+        'condition' => 'lte IE9',
+    ];
+}
