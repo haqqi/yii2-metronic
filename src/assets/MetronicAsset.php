dad2f2c63206c86b762728beeccced87e3200869
Haqqi <me@haqqi.net>
2017-05-07 21:11:05 +0700

    [change] font and icon asset placement

--- src/assets/MetronicAsset.php
+++ src/assets/MetronicAsset.php
@@ -7,6 +7,7 @@ use yii\web\AssetBundle;
 class MetronicAsset extends AssetBundle
 {
     public $depends = [
+        'haqqi\metronic\assets\core\FontIconAsset',
         'haqqi\metronic\assets\core\MainAsset',
         'haqqi\metronic\assets\core\IEAsset'
     ];
--- src/assets/core/MainAsset.php
+++ src/assets/core/MainAsset.php
@@ -10,13 +10,11 @@ class MainAsset extends AssetBundle
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset',
-        'rmrevin\yii\fontawesome\AssetBundle',
-        'mimicreative\assets\SimpleLineIconsAsset',
     ];
     
     public $css = [
         // main fonts
-        '//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i',
+        
         
     ];
 }
