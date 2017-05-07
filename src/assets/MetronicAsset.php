fc52c7f950a3bc9cfd184b45732c2e683841fb58
Haqqi <me@haqqi.net>
2017-05-07 20:29:05 +0700

    [add] metronic asset as main bundle

---
+++ src/assets/MetronicAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace haqqi\metronic\assets;
+
+use yii\web\AssetBundle;
+
+class MetronicAsset extends AssetBundle
+{
+    public $depends = [
+        'haqqi\metronic\assets\core\MainAsset',
+        'haqqi\metronic\assets\core\IEAsset'
+    ];
+}
