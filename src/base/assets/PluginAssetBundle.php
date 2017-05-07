1786a7d2f8e66c0e1bb00f020929edcbe23b9cca
Haqqi <me@haqqi.net>
2017-05-07 13:27:11 +0700

    [add] Plugin asset bundle base that publish each folder

---
+++ src/base/assets/PluginAssetBundle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace haqqi\metronic\base\assets;
+
+use haqqi\metronic\Metronic;
+use yii\base\InvalidConfigException;
+use yii\web\AssetBundle;
+
+class PluginAssetBundle extends AssetBundle
+{
+    public $pluginName;
+    
+    public $publishOptions = [
+        'except' => [
+            'plugins/'
+        ]
+    ];
+
+    public function __construct(array $config = [])
+    {
+        if($this->pluginName === null) {
+            throw new InvalidConfigException('pluginName must be set.');
+        }
+        
+        $this->sourcePath = Metronic::getComponent()->getAssetPath() . '/global/plugins/' . $this->pluginName;
+
+        parent::__construct($config);
+    }
+}
