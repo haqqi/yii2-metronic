7d66746ce7c80b1a81881442eeabf6b15e590b57
Haqqi <me@haqqi.net>
2017-05-07 14:23:44 +0700

    [change] GlobalPluginAssetBundle behaviors

--- src/base/assets/GlobalPluginAssetBundle.php
+++ src/base/assets/GlobalPluginAssetBundle.php
@@ -7,18 +7,16 @@ use yii\web\AssetBundle;
 
 class GlobalPluginAssetBundle extends AssetBundle
 {
+    public $publishOptions = [
+        'except' => [
+            '*/'
+        ]
+    ];
+    
     public function __construct(array $config = [])
     {
         $this->sourcePath = \Yii::$app->get(Metronic::$componentName)->getAssetPath() . '/global/plugins';
         
         parent::__construct($config);
-    }
-
-    public function init()
-    {
-        parent::init();
-
-        $this->publishOptions['except'] = ['*/'];
-        $this->publishOptions['only'] = array_merge($this->css, $this->js);
     }
 }
