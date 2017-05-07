73a2ee99d0cf8cc3ad99780b05bca34fddba1b86
Haqqi <me@haqqi.net>
2017-05-07 14:24:21 +0700

    [update] container for JqueryAsset

--- src/config/components.php
+++ src/config/components.php
@@ -1,20 +1,16 @@
 <?php
 
-use haqqi\metronic\Metronic;
+use haqqi\metronic\base\assets\GlobalPluginAssetBundle;
 use yii\web\View;
 
 return [
     'yii\web\JqueryAsset' => [
-        'sourcePath' => Yii::$app->get(Metronic::$componentName)->getAssetPath() . '/global/plugins',
-        'js'         => ['jquery.min.js'],
-        'jsOptions'  => ['position' => View::POS_HEAD],
-        'publishOptions' => [
-            'only' => [
-                'jquery.min.js'
-            ],
-            'except' => [
-                '*/'
-            ]
-        ]
+        'class'     => GlobalPluginAssetBundle::className(),
+        'js'        => [
+            'jquery.min.js'
+        ],
+        'jsOptions' => [
+            'position' => View::POS_HEAD
+        ],
     ],
 ];
