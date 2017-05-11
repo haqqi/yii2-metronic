2cf10178ed2c1468e90e72c87d569fd5e5b248ec
Haqqi <me@haqqi.net>
2017-05-12 02:05:54 +0700

    [add] JsCookieAsset

--- src/assets/core/PluginAsset.php
+++ src/assets/core/PluginAsset.php
@@ -7,6 +7,7 @@ use yii\web\AssetBundle;
 class PluginAsset extends AssetBundle
 {
     public $depends = [
+        'haqqi\metronic\assets\plugins\JsCookieAsset',
         'haqqi\metronic\assets\plugins\SlimScrollAsset'
     ];
 }
---
+++ src/assets/plugins/JsCookieAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace haqqi\metronic\assets\plugins;
+
+use haqqi\metronic\base\assets\GlobalPluginAssetBundle;
+
+class JsCookieAsset extends GlobalPluginAssetBundle
+{
+    public $js = [
+        'js.cookie.min.js'
+    ];
+}
