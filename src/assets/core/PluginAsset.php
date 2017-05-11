64264ac50a674144c9bdfe8bcd3434d0ad2d0e35
Haqqi <me@haqqi.net>
2017-05-12 02:01:25 +0700

    [add] CorePluginAsset

---
+++ src/assets/core/PluginAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace haqqi\metronic\assets\core;
+
+use yii\web\AssetBundle;
+
+class PluginAsset extends AssetBundle
+{
+    public $depends = [
+        'haqqi\metronic\assets\plugins\SlimScrollAsset'
+    ];
+}
