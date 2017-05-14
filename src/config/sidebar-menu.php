97f54949eb1597aeddc235cee24510dbeb052068
Haqqi <me@haqqi.net>
2017-05-15 02:22:59 +0700

    [add] example of sidebar menu

--- src/config/sidebar-menu.php
+++ src/config/sidebar-menu.php
@@ -1,14 +1,44 @@
 <?php
 
+use rmrevin\yii\fontawesome\FA;
+use yii\helpers\Html;
+
 return [
     [
         'label' => 'Dashboard',
-        'icon'  => '',
-        'badge' => '<span class="badge badge-pill badge-success">3</span>',
+        'icon'  => Html::tag('i', '', ['class' => 'icon-home']),
+//        'badge' => '<span class="badge badge-important">3</span>',
         'url'   => ['/metronic/dashboard/index']
     ],
     [
         'label' => 'General'
     ],
-    
+    [
+        'label' => 'Dashboard Style',
+        'icon' => FA::icon('diamond'),
+        'badge' => '<span class="badge badge-important">3</span>',
+        'items' => [
+            [
+                'label' => 'Dashboard v1',
+                'url'   => ['/metronic/dashboard/v1'],
+                'badge' => '<span class="badge bg-blue-steel">32</span>',
+            ],
+            [
+                'label' => 'Dashboard v2',
+                'url'   => ['/metronic/dashboard/v2'],
+            ],
+            [
+                'label' => 'Ecommerce',
+                'url'   => ['/metronic/dashboard/ecommerce'],
+            ],
+            [
+                'label' => 'Analytics',
+                'url'   => ['/metronic/dashboard/analytics'],
+            ],
+            [
+                'label' => 'Team',
+                'url'   => ['/metronic/dashboard/team'],
+            ],
+        ]
+    ],
 ];
