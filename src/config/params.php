83e3899cbb7209aa588f7f38c8688438919fc328
Haqqi <me@haqqi.net>
2017-05-15 05:16:12 +0700

    [add] default user params

--- src/config/params.php
+++ src/config/params.php
@@ -5,5 +5,6 @@ use yii\helpers\Html;
 return [
     'metronic' => [
         'footer' => 'Yii2 Metronic by ' . Html::a('Haqqi', 'http://blog.haqqi.net', ['target' => '_blank'])
-    ]
+    ],
+    'user.rememberMeDuration' => 3600 * 24 * 7 // 7 days
 ];
