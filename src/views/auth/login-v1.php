935748eaf56bb2ac70082db490ad6caa85e9db26
Haqqi <me@haqqi.net>
2017-05-16 18:55:45 +0700

    [finish] active form for main form

--- src/views/auth/login-v1.php
+++ src/views/auth/login-v1.php
@@ -7,6 +7,7 @@
 
 use haqqi\metronic\assets\core\PageLevelAsset;
 use haqqi\metronic\forms\LoginForm;
+use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 
 /** @var LoginForm $loginForm */
@@ -21,52 +22,82 @@ use yii\helpers\Html;
 
     <div class="content">
         <!-- BEGIN LOGIN FORM -->
-        <form class="login-form" action="index.html" method="post">
-            <h3 class="form-title font-green">Sign In</h3>
-            <div class="form-group">
-                <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-                <label class="control-label visible-ie8 visible-ie9">Username</label>
-                <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off"
-                       placeholder="Username" name="username"/></div>
-            <div class="form-group">
-                <label class="control-label visible-ie8 visible-ie9">Password</label>
-                <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off"
-                       placeholder="Password" name="password"/></div>
-            <div class="form-actions">
-                <button type="submit" class="btn green uppercase">Login</button>
-                <label class="rememberme check mt-checkbox mt-checkbox-outline">
-                    <input type="checkbox" name="remember" value="1"/>Remember
-                    <span></span>
-                </label>
-                <a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
-            </div>
-            <div class="login-options">
-                <h4>Or login with</h4>
-                <ul class="social-icons">
-                    <li>
-                        <a class="social-icon-color facebook" data-original-title="facebook" href="javascript:;"></a>
-                    </li>
-                    <li>
-                        <a class="social-icon-color twitter" data-original-title="Twitter" href="javascript:;"></a>
-                    </li>
-                    <li>
-                        <a class="social-icon-color googleplus" data-original-title="Goole Plus"
-                           href="javascript:;"></a>
-                    </li>
-                    <li>
-                        <a class="social-icon-color linkedin" data-original-title="Linkedin" href="javascript:;"></a>
-                    </li>
-                </ul>
-            </div>
+        <?php
+        $form = ActiveForm::begin([
+            'id'          => 'login-form',
+            'fieldConfig' => [
+                'template'     => "{label}\n{input}\n{hint}\n{error}",
+                'labelOptions' => [
+                    'class' => 'control-label visible-ie8 visible-ie9'
+                ],
+                //                'options'      => [
+                ////                    'class'       => 'form-group',
+                //                ],
+                'inputOptions' => [
+                    'class' => 'form-control form-control-solid placeholder-no-fix'
+                ],
+            ]
+        ]);
+        ?>
+        <h3 class="form-title font-green">Sign In</h3>
+        <?= $form->field($loginForm, 'email', [
+            'inputOptions' => [
+                'tabindex'    => 1,
+                'placeholder' => $loginForm->getAttributeLabel('email')
+            ]
+        ]); ?>
+        <?= $form->field($loginForm, 'password', [
+            'inputOptions' => [
+                'tabindex'    => 2,
+                'placeholder' => $loginForm->getAttributeLabel('password')
+            ]
+        ])->passwordInput(); ?>
+        <div class="form-actions">
+            <?= Html::submitButton('Login',
+                ['class' => 'btn green uppercase', 'tabindex' => 4]); ?>
 
-            <?php if ($loginForm->registerEnable) { ?>
-                <div class="create-account">
-                    <p>
-                        <a href="<?= Yii::$app->urlManager->createUrl($loginForm->registerUrl) ?>" id="register-btn" class="uppercase">Create an account</a>
-                    </p>
-                </div>
-            <?php } ?>
-        </form>
+            <?= $form->field($loginForm, 'rememberMe', [
+                'options' => [
+                    'tag' => null
+                ],
+                'checkboxTemplate' => "<label class='rememberme check mt-checkbox mt-checkbox-outline'>\n{input}\n{labelTitle}\n<span></span></label>"
+            ])->label('Remember')->checkbox(['tabindex' => 3]); ?>
+            <?php
+            if ($loginForm->forgotPasswordEnable) {
+                echo Html::a('Forgot password?', $loginForm->forgotPasswordUrl,
+                    ['id' => 'forget-password', 'class' => 'forget-password', 'tabindex' => 5]);
+            }
+            ?>
+        </div>
+        
+        <div class="login-options">
+            <h4>Or login with</h4>
+            <ul class="social-icons">
+                <li>
+                    <a class="social-icon-color facebook" data-original-title="facebook" href="javascript:;"></a>
+                </li>
+                <li>
+                    <a class="social-icon-color twitter" data-original-title="Twitter" href="javascript:;"></a>
+                </li>
+                <li>
+                    <a class="social-icon-color googleplus" data-original-title="Goole Plus"
+                       href="javascript:;"></a>
+                </li>
+                <li>
+                    <a class="social-icon-color linkedin" data-original-title="Linkedin" href="javascript:;"></a>
+                </li>
+            </ul>
+        </div>
+
+        <?php if ($loginForm->registerEnable) { ?>
+            <div class="create-account">
+                <p>
+                    <a href="<?= Yii::$app->urlManager->createUrl($loginForm->registerUrl) ?>" id="register-btn"
+                       class="uppercase">Create an account</a>
+                </p>
+            </div>
+        <?php } ?>
+        <?php ActiveForm::end(); ?>
         <!-- END LOGIN FORM -->
     </div>
 
