58168405169549234ff1fd8f10db466ceff0f8fb
Haqqi <me@haqqi.net>
2017-05-17 11:59:41 +0700

    [add] addon to pagelevelasset

--- src/assets/core/PageLevelAsset.php
+++ src/assets/core/PageLevelAsset.php
@@ -8,24 +8,57 @@ use yii\web\AssetBundle;
 class PageLevelAsset extends AssetBundle
 {
     public $css = [];
-    
+
     public $js = [];
-    
+
+    /**
+     * @var array addons assets
+     */
+    public $addons = [];
+
     public function __construct(array $config = [])
     {
         $this->sourcePath = Metronic::getComponent()->getAssetPath() . '/pages';
-        
+
         parent::__construct($config);
     }
-    
-    public static function login() {
+
+    public function init()
+    {
+        parent::init();
+
+        $this->_handleAddons();
+    }
+
+    public static function login()
+    {
         \Yii::$container->setDefinitions([
             self::className() => [
                 'css' => [
                     'css/login.min.css'
                 ],
-                'js' => []
+                'js'  => []
             ]
         ]);
+    }
+
+    private function _handleAddons()
+    {
+        $requestedRoute = \Yii::$app->requestedRoute;
+
+        if (array_key_exists($requestedRoute, $this->addons)) {
+
+            $additional = $this->addons[$requestedRoute];
+
+            if (array_key_exists('depends', $additional) && is_array($additional['depends'])) {
+                $this->depends = array_merge($this->css, $additional['depends']);
+            }
+            if (array_key_exists('js', $additional) && is_array($additional['js'])) {
+                $this->js = array_merge($this->js, $additional['js']);
+            }
+            if (array_key_exists('css', $additional) && is_array($additional['css'])) {
+                $this->css = array_merge($this->css, $additional['css']);
+            }
+        }
     }
 }
