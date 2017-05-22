17f7dfd45dba0b7afc1f24ed5d23db67c08bb7ea
L Shaf <shafry@mimicreative.net>
2017-05-22 16:40:56 +0700

    [fix] material form should have class "edited' when contain value

--- src/widgets/MaterialForm.php
+++ src/widgets/MaterialForm.php
@@ -1,9 +1,21 @@
 <?php
 namespace haqqi\metronic\widgets;
 
+use yii\web\View;
 use yii\bootstrap\ActiveForm;
 
 class MaterialForm extends ActiveForm
 {
     public $fieldClass = 'haqqi\metronic\widgets\MaterialField';
+
+    public function init()
+    {
+        parent::init();
+
+        $customJS = "$('.form-md-floating-label .form-control').each(function(){
+            if($(this).val() != '') $(this).addClass('edited');
+        });";
+
+        $this->getView()->registerJs($customJS, View::POS_READY, 'MaterialForm');
+    }
 }
