556e36f947c02d0c76eb4dc5c527d3b4146cdb31
L Shaf <shafry@mimicreative.net>
2017-05-22 10:28:43 +0700

    add metronic form

---
+++ src/widgets/MetronicForm.php
@@ -0,0 +1,9 @@
+<?php
+namespace haqqi\metronic\widgets;
+
+use yii\bootstrap\ActiveForm;
+
+class MetronicForm extends ActiveForm
+{
+    public $fieldClass = 'haqqi\metronic\widgets\MetronicField';
+}
