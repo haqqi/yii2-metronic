74f4b65a66ba5f8ce2cfd4f1bab545ff30e9dd71
L Shaf <shafry@mimicreative.net>
2017-05-23 11:29:26 +0700

    use js expression

--- src/widgets/MaterialForm.php
+++ src/widgets/MaterialForm.php
@@ -2,6 +2,7 @@
 namespace haqqi\metronic\widgets;
 
 use yii\web\View;
+use yii\web\JsExpression;
 use yii\bootstrap\ActiveForm;
 
 class MaterialForm extends ActiveForm
@@ -12,9 +13,9 @@ class MaterialForm extends ActiveForm
     {
         parent::init();
 
-        $customJS = "$('.form-md-floating-label .form-control').each(function(){
+        $customJS = new JsExpression("$('.form-md-floating-label .form-control').each(function(){
             if($(this).val() != '') $(this).addClass('edited');
-        });";
+        });");
 
         $this->getView()->registerJs($customJS, View::POS_READY, 'MaterialForm');
     }
