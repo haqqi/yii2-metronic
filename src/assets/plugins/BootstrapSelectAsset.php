03bf43034966bd4e4b0140cf522bc64e751ee73e
Haqqi <me@haqqi.net>
2017-06-11 13:28:17 +0700

    [new] BootstrapSelectAsset for plugin

---
+++ src/assets/plugins/BootstrapSelectAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace haqqi\metronic\assets\plugins;
+
+use haqqi\metronic\base\assets\PluginAssetBundle;
+
+class BootstrapSelectAsset extends PluginAssetBundle
+{
+    public $pluginName = 'bootstrap-select';
+
+    public $css = ['css/bootstrap-select.min.css'];
+
+    public $js = ['js/bootstrap-select.min.js'];
+}
