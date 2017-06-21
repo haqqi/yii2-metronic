8cb32b8b2559bb9b2f2d25de08cc15fa37e0e0d0
Haqqi <me@haqqi.net>
2017-06-21 16:24:38 +0700

    [change] Use metronic bootstrap asset instead of official bootstrap, because of the difference of color

---
+++ src/assets/plugins/BootstrapAsset.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace haqqi\metronic\assets\plugins;
+
+use haqqi\metronic\base\assets\PluginAssetBundle;
+
+class BootstrapAsset extends PluginAssetBundle
+{
+    public $pluginName = 'bootstrap';
+}
--- src/config/components.php
+++ src/config/components.php
@@ -1,5 +1,6 @@
 <?php
 
+use haqqi\metronic\assets\plugins\BootstrapAsset;
 use haqqi\metronic\assets\plugins\DataTableAsset;
 use haqqi\metronic\base\assets\GlobalPluginAssetBundle;
 use yii\web\View;
@@ -16,12 +17,14 @@ return [
     ],
 
     'yii\bootstrap\BootstrapAsset' => [
+        'class' => BootstrapAsset::className(),
         'css' => [
             'css/bootstrap.min.css'
         ]
     ],
     
     'yii\bootstrap\BootstrapPluginAsset' => [
+        'class' => BootstrapAsset::className(),
         'js'        => [
             'js/bootstrap.min.js'
         ],
